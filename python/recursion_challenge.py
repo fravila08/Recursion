c86316a8927ad0041da2b353224f1d34426472d9
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-06-09 14:36:20 -0700

    PY COMP

--- python/recursion_challenge.py
+++ python/recursion_challenge.py
@@ -1,11 +1,67 @@
-def factorial(x):
-	pass
+def factorial(n,answer=1):
+    answer=answer
+    if n==0:
+        return answer
+    else:
+        if n>0:
+            answer=answer * n
+            return factorial(n-1,answer)
 
-def palindrome(string):
-	pass
 
+def palindrome(word):
+    word=str(word)
+    word=word.lower()
+    word=''.join(e for e in word if e.isalnum())
+    if len(word)== 1 or len(word)==0:
+        return True
+    else:
+        if word[-1]==word[0]:
+            word=word[1:-1]
+            return palindrome(word)
+        else: 
+            return False
+ 
 def bottles(num):
-	pass
+    num2=num-1
+    if num == 2:
+        print(f'''{num} bottles of beer on the wall, {num} bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.
+1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.''')
+    if num == 1:
+        print(f'''1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall. 
+              No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.''')
+    elif num>2:
+        print(f'''{num} bottles of beer on the wall, {num} bottles of beer. Take one down and pass it around, {num2} bottles of beer on the wall.''')
+        return bottles(num2)
+    
+        
+bottles(10)
 
-def roman_num(num):
-	pass
+def roman_num(n, answer=''):
+    numerals={
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1,
+    }
+    
+    if n!=0:
+        for i in numerals:
+            if n>=numerals[i]:
+                answer+= i
+                return roman_num(n-numerals[i],answer)
+    else:
+        return answer
+        
+
+
+
--- python/recursion_spec.py
+++ python/recursion_spec.py
@@ -1,1 +1,29 @@
 # Write your unit tests here
+##This are all the test for Palindrome
+from recursion_challenge import roman_num
+from recursion_challenge import palindrome
+from recursion_challenge import factorial
+
+
+print("These are all the Palindrome Challenges")
+print(palindrome('racecar') == True)
+print(palindrome('Noon') == True)
+print(palindrome('ciVic') == True)
+print(palindrome('nice') == False)
+print(palindrome(434) == True)
+print(palindrome(123) == False)
+print(palindrome('bomb') == False)
+print(palindrome('Sore was I ere I saw Eros.') == True)
+print(palindrome('A man, a plan, a canal -- Panama') == True)
+
+
+print('These are the test cases for Roman Numeras')
+print(roman_num(1) == 'I')
+print(roman_num(3) == 'III')
+print(roman_num(4) == 'IV')
+print(roman_num(3944)=='MMMCMXLIV')
+
+print('These are the test cases for Factorial')
+print(factorial(8) == 40320)
+print(factorial(18) == 6402373705728000)
+print(factorial(45) == 119622220865480194561963161495657715064383733760000000000)
